e733d96417c7bd9296215ca7398f40629510ac68
hdsdi3g <admin@hd3g.tv>
2011-12-05 22:47:19 +0100

    Update example (Sample class)

--- src/hd3gtv/log2/demo/Sample.java
+++ src/hd3gtv/log2/demo/Sample.java
@@ -48,6 +48,16 @@ public class Sample {
 			var1   Value 1
 			var2   44
 			a file /etc/hosts	FRo 265 (2011/09/27 17:28:59,000)
+			
+			File description:
+			/etc/hosts = full path name
+			FRo = file in read only state (for Java process)
+			265 = size in octets
+			2011/09/27 17:28:59,000 = last edit/creation time
+			
+			FRW = file can to be read and write (for Java process)
+			Idem for DRW and DRo
+						
 		*/
 		
 		SampleThread samplethread1 = new SampleThread();
@@ -60,20 +70,20 @@ public class Sample {
 		
 		/*
 			RETURN
-			2011/12/05 15:36:31,095 INFO Sample "Start sample thread 1" main,1
-			 at hd3gtv.log2.demo.Sample.main(Sample.java:55)
-			info Nothing to tell
+		2011/12/02 22:45:02,916 INFO Sample "Start sample thread 1" main,1
+		 at hd3gtv.log2.demo.Sample.main(Sample.java:65)
+		info Nothing to tell
 
-			2011/12/05 15:36:31,096 INFO Sample "Start sample thread 2" main,1
-			 at hd3gtv.log2.demo.Sample.main(Sample.java:57)
-			info Nothing to tell
+		2011/12/02 22:45:02,916 INFO Sample "Start sample thread 2" main,1
+		 at hd3gtv.log2.demo.Sample.main(Sample.java:67)
+		info Nothing to tell
 
-			2011/12/05 15:36:31,096 INFO SampleThread "I run from a thread !" Thread-1,10
-			 at hd3gtv.log2.demo.SampleThread.run(SampleThread.java:14)
-			2011/12/05 15:36:31,096 INFO SampleThread "I run from a thread !" Thread-2,11
-			 at hd3gtv.log2.demo.SampleThread.run(SampleThread.java:14)
-			2011/12/05 15:36:31,097 INFO Sample "Done" main,1
-			 at hd3gtv.log2.demo.Sample.main(Sample.java:59)
+		2011/12/02 22:45:02,916 INFO SampleThread "I run from a thread !" Thread-1,10
+		 at hd3gtv.log2.demo.SampleThread.run(SampleThread.java:14)
+		2011/12/02 22:45:02,916 INFO SampleThread "I run from a thread !" Thread-2,11
+		 at hd3gtv.log2.demo.SampleThread.run(SampleThread.java:14)
+		2011/12/02 22:45:02,917 INFO Sample "Done" main,1
+		 at hd3gtv.log2.demo.Sample.main(Sample.java:69)
 		 
 		 */
 	}
